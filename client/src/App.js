e81fc5dacd58009b500ec2a5472c0195021d7df3
Łukasz Bartoś <lbartosm@gmail.com>
2018-12-27 16:43:26 +0100

    SASS preprocessor added; homepage prototype built

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import
+{
+	BrowserRouter as Router,
+	Link,
+	Route,
+	Switch,
+} from 'react-router-dom';
+// import logo from './logo.svg';
+import './App.scss';
+import GuestPanel from './components/Header/GuestPanel';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends Component
+{
+	state = {
+		response: '',
+		post: '',
+		responseToPost: '',
+	};
+	componentDidMount()
+	{
+		this.callApi()
+			.then(res => this.setState({ response: res.express }))
+			.catch(err => console.log(err));
+	}
+	callApi = async () =>
+	{
+		const response = await fetch('/api/hello');
+		const body = await response.json();
+		if (response.status !== 200) throw Error(body.message);
+		return body;
+	};
+	handleSubmit = async e =>
+	{
+		e.preventDefault();
+		const response = await fetch('/api/world', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ post: this.state.post }),
+		});
+		const body = await response.text();
+		this.setState({ responseToPost: body });
+	};
+
+
+	render()
+	{
+		return (
+			<Router>
+				<div>
+
+					<header className="App-header">
+						<div className="container d-flex flex-in-row">
+							<h1 className="logo">
+								<Link to="/">Ebookator</Link>
+							</h1>
+
+							<GuestPanel />
+						</div>
+					</header>
+					
+
+					<main>
+						<div className="container">
+							<p>{this.state.response}</p>
+							<form onSubmit={this.handleSubmit}>
+								<p>
+									<strong>Post to Server:</strong>
+								</p>
+								<input
+									type="text"
+									value={this.state.post}
+									onChange={e => this.setState({ post: e.target.value })}
+								/>
+								<button type="submit">Submit</button>
+							</form>
+							<p>{this.state.responseToPost}</p>
+						</div>
+
+					</main>
+
+
+					<footer
+						style={{
+							borderTop: "3px solid darkgray",
+							backgroundColor: "lightgray",
+							padding: "5px"
+						}}
+					>
+						<div className="container">
+							<p>Copyright &copy; {(new Date()).getFullYear()} by Łukasz Bartoś</p>
+						</div>
+					</footer>
+				</div>
+			</Router>
+		);
+	}
 }
-
 export default App;
